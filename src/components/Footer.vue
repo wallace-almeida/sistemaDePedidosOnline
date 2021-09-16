58cd665a4e693dff266c2710b83365583b52b6c9
Wallace Ferreira <wallacefdealmeida@hotmail.com>
2021-09-16 00:49:15 -0300

    Primeiro Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer id="footer">
+    <p>Make your Burger &copy; 2021</p>
+  </footer>
+</template>
+<script>
+export default {
+  nome: "Footer",
+};
+</script>
+<style scoped>
+#footer {
+  height: 200px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  color: #fcba03;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="nav">
+    <router-link id="logo-url" to="/">
+      <img id="logo" :src="logo" :alt="alt" />
+    </router-link>
+    <router-link to="/">Home </router-link>
+    <router-link to="/Pedidos">Pedidos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: ["logo", "alt"],
+};
+</script>
+
+<style scoped>
+#nav {
+  background-color: #222;
+  border-bottom: 4px solid #111;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+#nav #logo-url {
+  margin: auto;
+  margin-left: 0;
+}
+
+#logo {
+  width: 40px;
+  height: 40px;
+}
+
+#nav a {
+  color: #fcba03;
+  /* tirar o sublinhado dos links  */
+  text-decoration: none;
+  transition: 0.5s;
+  margin: 12px;
+}
+
+#nav a:hover {
+  /* ponteiro do mouse passa e fica branco  */
+  color: #fff;
+}
+</style>
